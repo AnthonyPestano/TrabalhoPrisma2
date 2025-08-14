d0f2143e77370adad76d3417e3c1a7fd7bcb8c14
AnthonyPestano <160540233+AnthonyPestano@users.noreply.github.com>
2025-08-14 15:56:03 -0300

    Update CustomerController.js

--- CustomerController.js
+++ CustomerController.js
@@ -2,13 +2,29 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 module.exports = {
-  async ListarCustomer(req, res) {
+  async ListarTodosCustomers(req, res) {
     try {
-      const Customer = await prisma.customer.findMany();
-      res.status(200).json(Customer);
+      const customer = await prisma.customer.findMany();
+      res.status(200).json(customer);
     } catch (error) {
       console.error('Erro ao listar clientes:', error);
       res.status(500).json({ error: 'Erro ao listar clientes' });
+    }
+  },
+
+  async ListarCustomer (req, res) {
+    const { id } = req.params;
+
+    try {
+      const customer = await prisma.customer.findUnique({
+        where: { id: parseInt(id) },
+      });
+      if (!customer) {
+        return res.status(404).json({ error: 'Cliente não encontrado' });
+      }
+      res.status(200).json(customer);
+    } catch (error) {
+      res.status(500).json({ error: 'Falha ao encontrar o cliente' });
     }
   },
 
@@ -20,14 +36,14 @@ module.exports = {
         return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
       }
 
-      const NovoCustomer = await prisma.customer.create({
+      const novoCustomer = await prisma.customer.create({
         data: {
           name,
           email,
         },
       });
 
-      res.status(201).json(NovoCustomer);
+      res.status(201).json(novoCustomer);
     } catch (error) {
       console.error('Erro ao cadastrar cliente:', error);
       res.status(500).json({ error: 'Erro ao cadastrar cliente' });
@@ -42,11 +58,11 @@ module.exports = {
         return res.status(400).json({ error: 'ID do cliente é obrigatório' });
       }
 
-      const Customer = await prisma.customer.delete({
+      const customer = await prisma.customer.delete({
         where: { id: parseInt(id) },
       });
 
-      res.status(200).json(Customer);
+      res.status(200).json(customer);
     } catch (error) {
       console.error('Erro ao deletar cliente:', error);
       res.status(500).json({ error: 'Erro ao deletar cliente' });
@@ -62,7 +78,7 @@ module.exports = {
         return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
       }
 
-      const CustomerAtualizado = await prisma.customer.update({
+      const customerAtualizado = await prisma.customer.update({
         where: { id: parseInt(id) },
         data: {
           name,
@@ -70,7 +86,7 @@ module.exports = {
         },
       });
 
-      res.status(200).json(CustomerAtualizado);
+      res.status(200).json(customerAtualizado);
     } catch (error) {
       console.error('Erro ao atualizar cliente:', error);
       res.status(500).json({ error: 'Erro ao atualizar cliente' });
