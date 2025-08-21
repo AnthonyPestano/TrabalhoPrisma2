72332450386b23cecb14bb46d29acaa514c90324
AnthonyPestano <160540233+AnthonyPestano@users.noreply.github.com>
2025-08-21 15:50:48 -0300

    Update OrderRoutes.js

--- OrderRoutes.js
+++ OrderRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 router.get('/ListarTodosOrders', OrderController.ListarTodosOrders);
 router.get('/ListarOrder/:id', OrderController.ListarOrder);
 router.delete('/DeletarOrder/:id', OrderController.DeletarOrder);
-router.post('/CadastrarOrder/:id', OrderController.CadastrarOrder);
-router.put('/AtualizarOrder/:id/:customerid', OrderController.AtualizarOrder); 
+router.post('/CadastrarOrder/', OrderController.CadastrarOrder);
+router.put('/AtualizarOrder/:id', OrderController.AtualizarOrder); 
 
 //falta atualizar e cadastrar orders, e fazer o listar todos orders listar apenas os orders de X customer, e não de todos, e o listar order listar só aquela order
 
